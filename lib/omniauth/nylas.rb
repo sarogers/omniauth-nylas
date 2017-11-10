cc3e447d86fbc680a77999dcbbeaca13a9f553fc
Kyle Bock <kyle.bock@salesloft.com>
2017-11-09 19:29:38 -0500

    Initial commit for omniauth-nylas strategy'

---
+++ lib/omniauth/nylas.rb
@@ -0,0 +1,7 @@
+require "omniauth/nylas/version"
+
+module Omniauth
+  module Nylas
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/nylas/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Nylas
+    VERSION = "0.1.0"
+  end
+end
